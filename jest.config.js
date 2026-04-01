147348f4ccc3d0fa0ab51185d435ff834201a48c
Vitor Augusto <vitoraugusto002@hotmail.com>
2026-03-31 22:17:02 -0300

    Enable Jest coverage, add pre-push hook enforcing 90% threshold

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 export default {
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    'js/config.js',
+    'js/state.js',
+    'js/upgrades.js',
+    'js/env.js',
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+      functions: 90,
+      branches: 90,
+      statements: 90,
+    },
+  },
 };
