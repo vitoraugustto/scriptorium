e18bb53a1999828b627afc3d35058a61c50b6a01
Vitor Augusto <vitoraugusto002@hotmail.com>
2026-04-03 16:08:54 -0300

    Add i18n infrastructure (t, setLocale, getLocale)

---
+++ js/i18n/index.js
@@ -0,0 +1,20 @@
+import en from './en.js';
+import ptBR from './pt-BR.js';
+
+const LOCALES = { en, 'pt-BR': ptBR };
+let _locale = localStorage.getItem('lang') || 'en';
+
+const t = (key, ...args) => {
+  let s = LOCALES[_locale][key] ?? LOCALES['en'][key] ?? key;
+  args.forEach((a, i) => { s = s.replaceAll(`{${i}}`, a); });
+  return s;
+};
+
+const setLocale = (locale) => {
+  _locale = locale;
+  localStorage.setItem('lang', locale);
+};
+
+const getLocale = () => _locale;
+
+export default { t, setLocale, getLocale };
