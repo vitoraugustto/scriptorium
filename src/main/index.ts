fcb2ffb19634ce0fe62a4b12fc7ba6581e94e29a
Vitor Augusto <vitoraugusto002@hotmail.com>
2026-04-03 18:25:31 -0300

    add electron main process and preload

---
+++ src/main/index.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { is } from '@electron-toolkit/utils';
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 860,
+    title: 'Scriptorium',
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false,
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    win.loadURL(process.env['ELECTRON_RENDERER_URL']);
+  } else {
+    win.loadFile(join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
