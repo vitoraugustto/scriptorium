12c979472ee180683a23c7333e1f99bd95824baa
Vitor Augusto <vitoraugusto002@hotmail.com>
2026-04-04 17:06:21 -0300

    Exclude e2e/ from Vitest to avoid Playwright test runner conflict

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: ['src/test/setup.ts'],
+    exclude: ['**/node_modules/**', 'e2e/**'],
     coverage: {
       provider: 'v8',
       include: ['src/renderer/js/**'],
